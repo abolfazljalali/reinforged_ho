2985205c31efd2f4de6cc7b1bd2d41251f8f5245
Aryan Jalali <Aryan@mwsp001.local>
2023-08-09 18:24:57 +0300

    add original code

--- src/blank_slate.py
+++ src/blank_slate.py
@@ -1,46 +1,45 @@
-import gym
 import torch
 import torch.nn as nn
-
-import pandas as pd
+import gym
 import numpy as np
+import pandas as pd
 
+import matplotlib.pyplot as plt
 from collections import OrderedDict
-
 from torch.utils.data import Dataset, DataLoader
 from stable_baselines3 import PPO
+from tqdm import tqdm
+
 from torch.nn import BCELoss
 from torch.optim import Adam
 
-import matplotlib.pyplot as plt
-from tqdm import tqdm
 
-class Monkey(nn.Module):
-    def __init__(self, in_features, out_feature) -> None:
-        super().__init__()
-        self.net = nn.Sequential(
-            OrderedDict([
-                ('layer_1', nn.Linear(in_features, 64)),
-                ('activation_1', nn.ReLU()),
-                ('layer_2', nn.Linear(64, 64)),
-                ('activation_2', nn.ReLU()),
-                ('layer_3', nn.Linear(64, out_feature)),
-            ])
-        )
+class BehaviouralCloningAgent(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(BehaviouralCloningAgent, self).__init__()
+        self.net = nn.Sequential(OrderedDict([
+            ('layer_1', nn.Linear(in_features, 64)),
+            ('activation_1', nn.ReLU()),
+            ('layer_2', nn.Linear(64, 64)),
+            ('activation_2', nn.ReLU()),
+            ('layer_3', nn.Linear(64, out_features))
+        ]))
+
         self.softmax = nn.Softmax()
         self.optimizer = Adam(self.net.parameters(), lr=3e-4)
         self.loss_fn = BCELoss()
 
-    def forward(self, X):
-        y = self.net(X)
+    def forward(self, x):
+        y = self.net(x)
         return self.softmax(y)
 
     def predict(self, obs):
         with torch.no_grad():
             y = self.net(obs)
-            action_prob = self.softmax(y)
-            return torch.argmax(action_prob)
-    
+            actions_prob = self.softmax(y)
+
+        return torch.argmax(actions_prob)
+
     def train(self, dataset, num_epochs=1):
         progress_bar = tqdm(range(num_epochs))
         data_loader = DataLoader(dataset, batch_size=64, shuffle=True)
@@ -48,42 +47,45 @@ class Monkey(nn.Module):
         for e in progress_bar:
             epoch_loss = []
             for obs, action in data_loader:
-                predicted_actions = self.forward(obs)
-                loss = self.loss_fn(predicted_actions, action)
+                predicted_action = self.forward(obs)
+                loss = self.loss_fn(predicted_action, action)
 
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
-                
+
                 epoch_loss.append(loss.item())
-            progress_bar.set_description(f'Epoch {e} Average Loss: {np.mean(epoch_loss)}')
-            losses.append(np.mean(epoch_loss))
+            progress_bar.set_description(f'Epoch {e} - Avg. Loss: {np.sum(epoch_loss) / len(epoch_loss)}')
+            losses.append(np.sum(epoch_loss) / len(epoch_loss))
+
+        plot_loss(losses)
 
 
-class MockingBox(Dataset):
-    def __init__(self) -> None:
-        super(MockingBox, self).__init__()
+class ObservationActionDataset(Dataset):
+    def __init__(self):
+        super(ObservationActionDataset, self).__init__()
         self.dataframe = pd.DataFrame(columns=['observation', 'action'])
 
     def __len__(self):
         return self.dataframe.shape[0]
 
-    def __getitem__(self, index):
-        record = self.dataframe.to_numpy()[index]
-        obs = record[0]
-        action = [0., 1.] if record[1] == 0 else [1., 0.]
-        print(obs, action)
+    def __getitem__(self, item: int):
+        row = self.dataframe.to_numpy()[item, :]
+        obs = row[0]
+        action = row[1]
+        action = [1., 0.] if action == 0 else [0., 1.]
+
         return torch.FloatTensor(obs), torch.FloatTensor(action)
 
     def store_trajectory(self, trajectory):
-        df = pd.DataFrame(trajectory, columns=['observations', 'action'])
+        df = pd.DataFrame(trajectory, columns=['observation', 'action'])
         self.dataframe = pd.concat([self.dataframe, df], axis=0)
 
 
 def gather_trajectories(expert, env, dataset, num_games=1, render=False):
     experience = []
-    for iteration in range(num_games):
-        obs,_ = env.reset()
+    for i in range(num_games):
+        obs, _ = env.reset()
         done = False
         while not done:
             action, _ = expert.predict(obs, deterministic=True)
@@ -91,33 +93,40 @@ def gather_trajectories(expert, env, dataset, num_games=1, render=False):
             obs, reward, done, _, info = env.step(action)
             if render:
                 env.render()
-        dataset.store_trajectory(experience)
 
-def agent_trajectories(agent, env, num_games=1):
-    for iteration in range(num_games):
-        obs,_ = env.reset()
+    dataset.store_trajectory(experience)
+
+
+def test_agent(agent, env, num_games=1):
+    for i in range(num_games):
+        obs, _ = env.reset()
         done = False
         total_reward = 0.0
         while not done:
             action = agent.predict(torch.FloatTensor(obs))
             obs, reward, done, _, info = env.step(action.numpy())
-            total_reward += reward
             env.render()
+            total_reward += reward
+        print(f'Game {i} - Total reward: {total_reward}')
 
-        print(f'Total reward: {total_reward}')
 
-def plot_losses(losses):
+def plot_loss(losses):
     plt.plot(range(len(losses)), losses)
-    plt.xlabel('Epochs')
+    plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.show()
 
+
 if __name__ == '__main__':
     env = gym.make('CartPole-v1', render_mode='human')
-    expert = PPO.load('model/expert_10k')
-    agent = Monkey(in_features=4, out_feature=2)
-    dataset = MockingBox()
+    expert = PPO.load("model/expert_10k")
+    dataset = ObservationActionDataset()
+    agent = BehaviouralCloningAgent(in_features=4, out_features=2)
+
     gather_trajectories(expert, env, dataset, num_games=20)
-    agent_trajectories(agent, env, 10)
-    agent.train(dataset, 25)
-    agent_trajectories(agent, env, 10)
+
+    test_agent(agent, env, num_games=10)
+
+    agent.train(dataset, num_epochs=15)
+
+    test_agent(agent, env, num_games=10)
